751ec85f95af5e5a71ac50fde2b6506ae71c4593
wynnodonnell <55889686+wynnodonnell@users.noreply.github.com>
2020-01-28 18:36:20 -0500

    Added Basic Folder Stuff

---
+++ functions.js
@@ -0,0 +1,10 @@
+//My first lines of function code
+
+
+function doSomething(price,howMany,name) {
+    let total = 0;
+    total = price*howMany;
+    console.log('Good day '+name +' your total is '+total);
+}
+
+//doSomething();
